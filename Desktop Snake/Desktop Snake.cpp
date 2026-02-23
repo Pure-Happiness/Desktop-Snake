cbf0a5a07df2a98fe35241177da0622a76723a5f
Enigma <Pure-Happiness@outlook.com>
2026-02-23 17:52:19 +0800

    Before enabling control

--- Desktop Snake/Desktop Snake.cpp
+++ Desktop Snake/Desktop Snake.cpp
@@ -1,105 +1,179 @@
-﻿#include "framework.h"
-#include "Desktop Snake.h"
+﻿#include "resource.h"
+#include "targetver.h"
+#include <Shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
+#include <ExDisp.h>
+#include <ShlObj.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <atomic>
+#include <cassert>
+#include <cstdlib>
+#include <optional>
+#include <queue>
+#include <vector>
 
-constexpr static auto MAX_LOADSTRING = 128, WM_TRAYICON = WM_USER;
+static std::atomic_char dialog_result;
 
-static HINSTANCE hInst;
-
-static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_TRAYICON:
-		if (lParam == WM_RBUTTONUP || lParam == WM_LBUTTONUP)
-		{
-			SetForegroundWindow(hWnd);
-			POINT pt;
-			GetCursorPos(&pt);
-			TrackPopupMenu(GetSubMenu(LoadMenu(hInst, MAKEINTRESOURCE(IDC_DESKTOPSNAKE)), 0), 0, pt.x, pt.y, 0, hWnd, nullptr);
-			return 0;
-		}
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDM_EXIT)
-		{
-			DestroyWindow(hWnd);
-			return 0;
-		}
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
+static INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam,
+                                   LPARAM) {
+  switch (message) {
+  case WM_INITDIALOG:
+    return (INT_PTR)TRUE;
+  case WM_COMMAND:
+    switch (LOWORD(wParam)) {
+    case IDOK:
+      dialog_result = 'T';
+      goto notify;
+    case IDCANCEL:
+      dialog_result = 'F';
+      goto notify;
+    }
+  }
+  return (INT_PTR)FALSE;
+notify:
+  dialog_result.notify_one();
+  EndDialog(hDlg, LOWORD(wParam));
+  return (INT_PTR)TRUE;
 }
 
-static void Move()
-{
-	if (IShellWindows* psw; SUCCEEDED(CoCreateInstance(CLSID_ShellWindows, nullptr, CLSCTX_ALL, IID_PPV_ARGS(&psw))))
-	{
-		VARIANT varLoc{ VT_EMPTY };
-		long hwnd;
-		IDispatch* pdisp;
-		assert(psw->FindWindowSW(&varLoc, &varLoc, SWC_DESKTOP, &hwnd, SWFO_NEEDDISPATCH, &pdisp) == S_OK);
-		IShellBrowser* psb;
-		assert(SUCCEEDED(IUnknown_QueryService(pdisp, SID_STopLevelBrowser, IID_PPV_ARGS(&psb))));
-		IShellView* psv;
-		assert(SUCCEEDED(psb->QueryActiveShellView(&psv)));
-		HWND hWnd;
-		assert(SUCCEEDED(psv->GetWindow(&hWnd)));
-		RECT rect;
-		assert(GetClientRect(hWnd, &rect));
-		UINT dpi = GetDpiForWindow(hWnd), width = rect.right * dpi / 96, height = rect.bottom * dpi / 96;
-		IFolderView2* pfv;
-		assert(SUCCEEDED(psv->QueryInterface(IID_PPV_ARGS(&pfv))));
-		POINT pt;
-		assert(SUCCEEDED(pfv->GetSpacing(&pt)));
-	}
+static bool WaitDialog(LPCWSTR lpTemplate) {
+  dialog_result = 'N';
+  DialogBox(nullptr, lpTemplate, nullptr, DialogProc);
+  dialog_result.wait('N');
+  return dialog_result == 'T';
 }
 
-static void Control()
-{
-
-}
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
-{
-	hInst = hInstance;
-	WCHAR szTitle[MAX_LOADSTRING], szWindowClass[MAX_LOADSTRING];
-	my_assert(LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING) > 0);
-	my_assert(LoadString(hInstance, IDC_DESKTOPSNAKE, szWindowClass, MAX_LOADSTRING) > 0);
-	HICON hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DESKTOPSNAKE));
-	my_assert(hIcon);
-	{
-		HANDLE hMutex = CreateMutex(nullptr, TRUE, szWindowClass);
-		my_assert(hMutex);
-		if (GetLastError() == ERROR_ALREADY_EXISTS)
-			return 0;
-	}
-	{
-		WNDCLASSW wcex{ .lpfnWndProc = WndProc, .hInstance = hInstance, .lpszClassName = szWindowClass };
-		my_assert(RegisterClass(&wcex));
-	}
-	{
-		HWND hWnd = CreateWindow(szWindowClass, nullptr, 0, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-		my_assert(hWnd);
-		NOTIFYICONDATA nid{
-			.cbSize = sizeof(NOTIFYICONDATA),
-			.hWnd = hWnd,
-			.uID = 0,
-			.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP,
-			.uCallbackMessage = WM_TRAYICON,
-			.hIcon = hIcon
-		};
-		std::memcpy(nid.szTip, szTitle, sizeof(nid.szTip));
-		assert(Shell_NotifyIcon(NIM_ADD, &nid));
-	}
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	return (int)msg.wParam;
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int) {
+  {
+    HANDLE hMutex = CreateMutex(nullptr, TRUE, L"DESKTOPSNAKE");
+    assert(hMutex);
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+      return 0;
+  }
+  CoInitialize(nullptr);
+  IShellWindows *psw;
+  assert(SUCCEEDED(CoCreateInstance(CLSID_ShellWindows, nullptr, CLSCTX_ALL,
+                                    IID_PPV_ARGS(&psw))));
+  IDispatch *pdisp;
+  {
+    VARIANT varLoc{VT_EMPTY};
+    long temp;
+    assert(psw->FindWindowSW(&varLoc, &varLoc, SWC_DESKTOP, &temp,
+                             SWFO_NEEDDISPATCH, &pdisp) == S_OK);
+  }
+  IShellBrowser *psb;
+  assert(SUCCEEDED(
+      IUnknown_QueryService(pdisp, SID_STopLevelBrowser, IID_PPV_ARGS(&psb))));
+  IShellView *psv;
+  assert(SUCCEEDED(psb->QueryActiveShellView(&psv)));
+  IFolderView2 *pfv;
+  assert(SUCCEEDED(psv->QueryInterface(IID_PPV_ARGS(&pfv))));
+  int cItems;
+  assert(SUCCEEDED(pfv->ItemCount(SVGIO_ALLVIEW, &cItems)));
+  if (cItems <= 0) {
+    WCHAR message[64];
+    assert(LoadString(hInstance, IDS_NO_ICON, message, 64) > 0);
+    MessageBox(nullptr, message, nullptr, MB_OK);
+    return 0;
+  }
+  if (cItems < 10 && !WaitDialog(MAKEINTRESOURCE(IDD_FEWICONS)))
+    return 0;
+  DWORD dwFlags;
+  assert(SUCCEEDED(pfv->GetCurrentFolderFlags(&dwFlags)));
+  constexpr DWORD requiredFlags = FWF_AUTOARRANGE | FWF_SNAPTOGRID;
+  assert(SUCCEEDED(pfv->SetCurrentFolderFlags(requiredFlags, 0)));
+  POINT pt;
+  assert(SUCCEEDED(pfv->GetSpacing(&pt)));
+  PITEMID_CHILD *const apidl = new PITEMID_CHILD[cItems];
+  auto set_icon = [pfv, apidl](int i, POINT p) {
+    assert(SUCCEEDED(pfv->SelectAndPositionItems(1, (LPCITEMIDLIST *)apidl + i,
+                                                 &p, SVSI_POSITIONITEM)));
+  };
+  POINT *const apt = new POINT[cItems];
+  for (int i{}; i < cItems; ++i) {
+    assert(SUCCEEDED(pfv->Item(i, apidl + i)));
+    assert(SUCCEEDED(pfv->GetItemPosition(apidl[i], apt + i)));
+  }
+  HWND hWnd;
+  assert(SUCCEEDED(psv->GetWindow(&hWnd)));
+  RECT rect;
+  assert(GetClientRect(hWnd, &rect));
+  const UINT dpi = GetDpiForWindow(hWnd);
+  const POINT screen{.x = rect.right * (LONG)dpi / 96 / pt.x,
+                     .y = rect.bottom * (LONG)dpi / 96 / pt.y};
+  constexpr POINT dir[]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+  const POINT DIR[]{{pt.x, 0}, {0, pt.y}, {-pt.x, 0}, {0, -pt.y}};
+  struct Unit {
+    LONG x, y, X, Y;
+    int index;
+  };
+  std::srand(std::time(nullptr));
+  while (true) {
+    for (int i = 1; i < cItems; ++i)
+      set_icon(i, {-pt.x, -pt.y});
+    set_icon(0, {});
+    std::size_t d = 0, banned = 2;
+    std::vector<Unit> body{Unit{}};
+    std::queue<Unit> food;
+    std::vector<std::vector<bool>> occupied(screen.x,
+                                            std::vector<bool>(screen.y));
+    occupied[0][0] = true;
+    int next_index = 1;
+    std::optional<Unit> next_food;
+    auto set_food = [&occupied, &next_index, &next_food, &set_icon, cItems, pt,
+                     screen] {
+      if (next_index < cItems) {
+        LONG x = std::rand() % screen.x, y = std::rand() % screen.y;
+        while (occupied[x][y])
+          x = std::rand() % screen.x, y = std::rand() % screen.y;
+        LONG X = x * pt.x, Y = y * pt.y;
+        next_food = {x, y, X, Y, next_index};
+        set_icon(next_index++, {.x = X, .y = Y});
+      } else
+        next_food = std::nullopt;
+    };
+    set_food();
+    while (true) {
+      Sleep(1000); // For debug. Decrease the time interval later.
+      banned = d ^ 2;
+      Unit old_tail = body.back();
+      for (auto it = body.rbegin(), nit = next(it); nit != body.rend();
+           it = nit++) {
+        it->x = nit->x, it->y = nit->y;
+        set_icon(it->index, {.x = it->X = nit->X, .y = it->Y = nit->Y});
+      }
+      if (!food.empty()) {
+        auto &f = food.front();
+        if (old_tail.x == f.x && old_tail.y == f.y) {
+          body.push_back(f);
+          food.pop();
+          goto no_remove_end;
+        }
+      }
+      occupied[old_tail.x][old_tail.y] = false;
+    no_remove_end:
+      Unit &head = body.front();
+      if ((head.x += dir[d].x) >= 0 && head.x < screen.x &&
+          (head.y += dir[d].y) >= 0 && head.y < screen.y)
+        if (auto cell = occupied[head.x][head.y]; !cell) {
+          cell = true;
+          set_icon(head.index,
+                   {.x = head.X += DIR[d].x, .y = head.Y += DIR[d].y});
+          if (next_food)
+            if (auto &nf = *next_food; head.x == nf.x && head.y == nf.y) {
+              food.push(*next_food);
+              set_food();
+            }
+          continue;
+        }
+      break;
+    }
+    if (!WaitDialog(MAKEINTRESOURCE(IDD_GAMEOVER)))
+      break;
+  }
+  for (int i{}; i < cItems; ++i)
+    set_icon(i, apt[i]);
+  pfv->SetCurrentFolderFlags(requiredFlags, dwFlags);
+  return 0;
 }
